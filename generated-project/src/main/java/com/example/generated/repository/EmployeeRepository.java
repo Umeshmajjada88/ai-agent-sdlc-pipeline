5552def7b76f64b14e6d70e9d7cd25f59588c03a
Umesh <majjada.umesh.min21@itbhu.ac.in>
2026-05-27 14:19:37 +0530

    Dynamic class name generation

--- generated-project/src/main/java/com/example/generated/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springaidemo.repository;
-
-import com.example.springaidemo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
